9091fccb3258496647361790a8f08cf6c5eefbb3
MarianaSantana04 <marianasantanacosta2020@gmail.com>
2025-04-10 09:59:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Js significa java script
+
+var sEf = ["Aktriz", "WandaVision", "Historia cruzadas", "Love101", "Vingadores:Era de Ultron","Vingadores:Ultimato", "La la land", 
+"E se fosse verdade", "Yargi", "Rainha da lágrimas", "Nadas Suspeitos", "Adolescência","Uma linda Mulher", "RRR", "Barbie, escola de princesa" ]
+
+var sEfEscolhida = parseInt(prompt("Digite um número de 0 a 14"))
+
+document.write("Série ou filme escolhido: " + sEf[sEfEscolhida])
